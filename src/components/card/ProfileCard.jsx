a732dedf2254c3736b68a180ddfe57fb00f08c95
Joseph <jxlove2020@gmail.com>
2022-07-07 04:34:50 +0900

    카드 형태 컴포넌트

---
+++ src/components/card/ProfileCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Card";
+function ProfileCard(props) {
+	return (
+		<div>
+			<Card title="Joseph" backgroundColor="#4ea042">
+				<p>안녕하세요 조셉입니다.</p>
+				<p>많은 관심 부탁드립니다.</p>
+			</Card>
+		</div>
+	);
+}
+
+export default ProfileCard;
